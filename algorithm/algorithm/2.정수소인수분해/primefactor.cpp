bb088f31a963f645a1561ce7d2570a8c37dc650a
hogbal <hogbal31@gmail.com>
2020-09-02 21:33:59 +0900

    2.정수소인수분해

--- algorithm/algorithm/2.정수소인수분해/primefactor.cpp
+++ algorithm/algorithm/2.정수소인수분해/primefactor.cpp
@@ -5,6 +5,37 @@ using namespace std;
 ifstream inp("primefactor.inp");
 ofstream out("primefactor.out");
 
-int main(void) {
+int prime(){
+    int num;
+    int sum = 0;
+    int start = 2;
+    inp >> num;
     
+    while(num != 1) {
+        if(num % start == 0) {
+            if(sum == 0 && start == num) {
+                return -1;
+            }
+            num /= start;
+            sum += start;
+        }
+        else {
+            start++;
+        }
+    }
+    return sum;
+}
+
+int main(void) {
+    int count;
+    inp >> count;
+    for(int i=0;i<count;i++) {
+        int result = prime();
+        if(result == -1) {
+            out << "Prime Number" << endl;
+        }
+        else {
+            out << result << endl;
+        }
+    }
 }
