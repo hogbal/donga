042bef5d28445b82b8ca25681a009bb69cd0aca8
hogbal <hogbal31@gmail.com>
2020-09-26 01:23:14 +0900

    cube

--- algorithm/algorithm/10.Cube Painting/cube.cpp
+++ algorithm/algorithm/10.Cube Painting/cube.cpp
@@ -6,12 +6,40 @@ using namespace std;
 ifstream inp("cube.inp");
 ofstream out("cube.out");
 
-void change_top(string &dice,int num) {
-    string temp = dice;
-    dice[0] = temp[1];
-    dice[4] = temp[0];
-    dice[5] = temp[4];
-    dice[1] = temp[5];
+void change_top(string &dice, int num, string dice_save) {
+    if(num == 0) {
+        return;
+    }
+    else if(num == 1){
+        dice[0] = dice_save[1];
+        dice[1] = dice_save[5];
+        dice[5] = dice_save[4];
+        dice[4] = dice_save[0];
+    }
+    else if(num == 2) {
+        dice[0] = dice_save[2];
+        dice[2] = dice_save[5];
+        dice[5] = dice_save[3];
+        dice[3] = dice_save[0];
+    }
+    else if(num == 3) {
+        dice[0] = dice_save[3];
+        dice[3] = dice_save[5];
+        dice[5] = dice_save[2];
+        dice[2] = dice_save[0];
+    }
+    else if(num == 4) {
+        dice[0] = dice_save[4];
+        dice[4] = dice_save[5];
+        dice[5] = dice_save[1];
+        dice[1] = dice_save[0];
+    }
+    else if(num == 5) {
+        dice[0] = dice_save[5];
+        dice[5] = dice_save[0];
+        dice[1] = dice_save[4];
+        dice[4] = dice_save[1];
+    }
 }
 
 void rotation(string &dice) {
@@ -23,8 +51,9 @@ void rotation(string &dice) {
 }
 
 bool compare_dice(string dice1, string dice2) {
+    string dice2_save = dice2;
     for(int i=0;i<6;i++) {
-        change_top(dice2,i);
+        change_top(dice2,i,dice2_save);
         for(int j=0;j<4;j++) {
             rotation(dice2);
             if(dice1 == dice2) return true;
