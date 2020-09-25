b3cf603ea63362569349406391ef4f00f9e095b4
hogbal <hogbal31@gmail.com>
2020-09-26 00:35:46 +0900

    Cube Painting

---
+++ algorithm/algorithm/10.Cube Painting/cube.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+ifstream inp("cube.inp");
+ofstream out("cube.out");
+
+void change_top(string &dice,int num) {
+    string temp = dice;
+    dice[0] = temp[1];
+    dice[4] = temp[0];
+    dice[5] = temp[4];
+    dice[1] = temp[5];
+}
+
+void rotation(string &dice) {
+    string temp = dice;
+    dice[1] = temp[2];
+    dice[2] = temp[4];
+    dice[3] = temp[1];
+    dice[4] = temp[3];
+}
+
+bool compare_dice(string dice1, string dice2) {
+    for(int i=0;i<6;i++) {
+        change_top(dice2,i);
+        for(int j=0;j<4;j++) {
+            rotation(dice2);
+            if(dice1 == dice2) return true;
+        }
+    }
+    return false;
+}
+
+int cube(){
+    string input_st;
+    inp >> input_st;
+    
+    string dice1,dice2;
+    dice1 = input_st.substr(0,6);
+    dice2 = input_st.substr(6,6);
+    
+    if(dice1 == dice2) {
+        bool check = true;
+        char com = dice1[0];
+        for(int i=1;i<6;i++) {
+            if(com != dice1[i]) {
+                check = false;
+            }
+        }
+        if(check) return -1;
+        else return 1;
+    }
+    
+    if(compare_dice(dice1,dice2)){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main(void) {
+    while(1) {
+        int check = cube();
+        if(check == 0) {
+            out << "FALSE" << endl;
+        }
+        else if(check == 1) {
+            out << "TRUE" << endl;
+        }
+        else {
+            out << "TRUE" << endl;
+            break;
+        }
+    }
+}
