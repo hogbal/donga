c3222408f38b8a241c35faba9fb541582b1219a4
hogbal <hogbal31@gmail.com>
2020-09-30 18:46:10 +0900

    cube

--- algorithm/algorithm/10.Cube Painting/cube.cpp
+++ algorithm/algorithm/10.Cube Painting/cube.cpp
@@ -6,11 +6,15 @@ using namespace std;
 ifstream inp("cube.inp");
 ofstream out("cube.out");
 
-void change_top(string &dice, int num, string dice_save) {
-    
+void rotation1(string &dice) {     //2->1 방향으로 회전
+    string temp = dice;
+    dice[0] = temp[1];
+    dice[1] = temp[5];
+    dice[5] = temp[4];
+    dice[4] = temp[0];
 }
 
-void rotation(string &dice) {
+void rotation2(string &dice) {   //3->2 방향으로 회전
     string temp = dice;
     dice[1] = temp[2];
     dice[2] = temp[4];
@@ -18,15 +22,31 @@ void rotation(string &dice) {
     dice[4] = temp[3];
 }
 
+void rotation3(string &dice) {    //3->1 방향으로 회전
+    string temp = dice;
+    dice[0] = temp[2];
+    dice[2] = temp[5];
+    dice[5] = temp[3];
+    dice[3] = temp[0];
+}
+
 bool compare_dice(string dice1, string dice2) {
-    string dice2_save = dice2;
-    for(int i=0;i<6;i++) {
-        change_top(dice2,i,dice2_save);
+    for(int i=0;i<4;i++) {
+        rotation1(dice2);
         for(int j=0;j<4;j++) {
-            rotation(dice2);
+            rotation2(dice2);
             if(dice1 == dice2) return true;
         }
     }
+    rotation3(dice2);
+    for(int i=0;i<4;i++) {
+        rotation1(dice2);
+        for(int j=0;j<4;j++) {
+            rotation2(dice2);
+            if(dice1 == dice2) return true;
+        }
+    }
+    
     return false;
 }
 
@@ -49,6 +69,21 @@ int cube(){
         if(check) return -1;
         else return 1;
     }
+    
+    int r1=0,g1=0,b1=0;
+    int r2=0,g2=0,b2=0;
+    
+    for(int i=0;i<6;i++) {
+        if(dice1[i] == 'r') r1++;
+        else if(dice1[i] == 'g') g1++;
+        else b1++;
+        
+        if(dice2[i] == 'r') r2++;
+        else if(dice2[i] == 'g') g2++;
+        else b2++;
+    }
+    
+    if(r1!=r2 || g1!=g2 || b1!=b2) return 0;
     
     if(compare_dice(dice1,dice2)){
         return 1;
