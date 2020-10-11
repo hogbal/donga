887ecb098e0284a1f4bf3892d13f809189bf2cb6
hogbal <hogbal31@gmail.com>
2020-10-11 16:58:09 +0900

    path3

--- algorithm/algorithm/14.격자경로3/path.cpp
+++ algorithm/algorithm/14.격자경로3/path.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <stack>
 #include <utility>
 using namespace std;
@@ -15,6 +16,7 @@ typedef struct {
     int *num_case;
     int M_w;
     int N_w;
+    pair<int, int> *output_path;
 }Edge;
 
 void input_mat(Edge *e[]) {    //입력받는 함수
@@ -47,17 +49,22 @@ void w_cal(Edge *e[], int index_i, int index_j){
     else if(index_i == 0 && index_j == 1) { //[0,1]일 경우 [0,0]의 N_w값만 더해주면 된다.
         if(e[0][0].num != -1) {
             e[index_i][index_j].cost[0] = e[0][0].N_w;
+            e[index_i][index_j].output_path[0] = make_pair(0, 0);
         }
         else {
             e[index_i][index_j].cost[1] = e[0][0].N_w;
+            e[index_i][index_j].output_path[1] = make_pair(0, 0);
+            
         }
     }
     else if(index_i == 1 && index_j == 0) { //[1,0]일 경우 [0,0]의 M_w값만 더해주면 된다.
         if(e[0][0].num != -1) {
             e[index_i][index_j].cost[0] = e[0][0].M_w;
+            e[index_i][index_j].output_path[0] = make_pair(0, 0);
         }
         else {
             e[index_i][index_j].cost[1] = e[0][0].M_w;
+            e[index_i][index_j].output_path[1] = make_pair(0, 0);
         }
     }
     else if(index_i == 0) { //맨 윗 줄일 경우 왼쪽 노드의 N_w값을 더해주면 된다.
@@ -67,6 +74,7 @@ void w_cal(Edge *e[], int index_i, int index_j){
             }
             else {
                 e[index_i][index_j].cost[i] = e[index_i][index_j-1].N_w + e[index_i][index_j-1].cost[i];
+                e[index_i][index_j].output_path[i] = make_pair(index_i, index_j-1);
             }
         }
     }
@@ -77,6 +85,7 @@ void w_cal(Edge *e[], int index_i, int index_j){
             }
             else {
                 e[index_i][index_j].cost[i] = e[index_i-1][index_j].M_w + e[index_i-1][index_j].cost[i];
+                e[index_i][index_j].output_path[i] = make_pair(index_i-1, index_j);
             }
         }
     }
@@ -87,16 +96,20 @@ void w_cal(Edge *e[], int index_i, int index_j){
             }
             else if(e[index_i-1][index_j].cost[i] == 0) {   //한 곳이 0일경우 다른 한곳의 값을 이용
                 e[index_i][index_j].cost[i] = e[index_i][index_j-1].N_w + e[index_i][index_j-1].cost[i];
+                e[index_i][index_j].output_path[i] = make_pair(index_i, index_j-1);
             }
             else if(e[index_i][index_j-1].cost[i] == 0) {   //한 곳이 0일경우 다른 한곳의 값을 이용
                 e[index_i][index_j].cost[i] = e[index_i-1][index_j].M_w + e[index_i-1][index_j].cost[i];
+                e[index_i][index_j].output_path[i] = make_pair(index_i-1, index_j);
             }
             else { //왼쪽노드와 윗 노드의 가중치를 계산해 더 작은 값으로 할당한다.
                 if(e[index_i][index_j-1].cost[i]+e[index_i][index_j-1].N_w > e[index_i-1][index_j].cost[i]+e[index_i-1][index_j].M_w) {
                     e[index_i][index_j].cost[i] = e[index_i-1][index_j].M_w + e[index_i-1][index_j].cost[i];
+                    e[index_i][index_j].output_path[i] = make_pair(index_i-1, index_j);
                 }
                 else {
                     e[index_i][index_j].cost[i] = e[index_i][index_j-1].N_w + e[index_i][index_j-1].cost[i];
+                    e[index_i][index_j].output_path[i] = make_pair(index_i, index_j-1);
                 }
             }
         }
@@ -129,81 +142,45 @@ void arr_slide(Edge *e[], int index_i, int index_j){ //노드가 num==-1일 경�
         if(i == 0) {
             e[index_i][index_j].num_case[0] = 0;
             e[index_i][index_j].cost[0] = 0;
+            e[index_i][index_j].output_path[0] = make_pair(-1, -1);
         }
         else {
             e[index_i][index_j].num_case[i] = e[index_i][index_j].num_case[i-1];
             e[index_i][index_j].cost[i] = e[index_i][index_j].cost[i-1];
+            e[index_i][index_j].output_path[i] = e[index_i][index_j].output_path[i-1];
         }
     }
 }
 
-void out_stack(Edge *e[], int k){
+void output_answer(Edge *e[], int k){
+    stack<pair<int, int> > s;
     int index_i = M-1, index_j = N-1;
-    stack<pair<int, int> > v;
-    int k_com = k;
+    s.push(make_pair(M-1, N-1));
     
-    while(1) {
+    while(1){
         if(index_i == 0 && index_j == 0) {
-            v.push(make_pair(0,0));
             break;
         }
         else {
-            v.push(make_pair(index_i, index_j));
-            if(index_i == 0) {
-                index_j--;
-            }
-            else if(index_j == 0) {
-                index_i--;
+            if(e[index_i][index_j].output_path[k].first != -1 && e[index_i][index_j].output_path[k].second != -1) {
+                s.push(e[index_i][index_j].output_path[k]);
             }
             else {
-                if(e[index_i-1][index_j].cost[k] == 0) {
-                    index_j--;
+                if(!(s.top().first == 0 && s.top().second == 0)) {
+                    s.push(make_pair(0, 0));
                 }
-                else if(e[index_i][index_j-1].cost[k] == 0) {
-                    index_i--;
-                }
-                else if(e[index_i-1][index_j].cost[k] > e[index_i][index_j-1].cost[k]){
-                    if(e[index_i][index_j-1].N_w+e[index_i][index_j-1].cost[k] != e[index_i][index_j].cost[k_com]) {
-                        index_i--;
-                    }
-                    else {
-                        index_j--;
-                    }
-                }
-                else if(e[index_i-1][index_j].cost[k] == e[index_i][index_j-1].cost[k]){
-                    if(e[index_i-1][index_j].M_w+e[index_i-1][index_j].cost[k] != e[index_i][index_j].cost[k_com]) {
-                        index_j--;
-                    }
-                    else if(e[index_i][index_j-1].N_w+e[index_i][index_j-1].cost[k] != e[index_i][index_j].cost[k_com]) {
-                        index_i--;
-                    }
-                    else {
-                        index_j--;
-                    }
-                }
-                else {
-                    if(e[index_i-1][index_j].M_w+e[index_i-1][index_j].cost[k] != e[index_i][index_j].cost[k_com]) {
-                        index_j--;
-                    }
-                    else {
-                        index_i--;
-                    }
-                }
+                break;
             }
-            
-            if(e[index_i][index_j].num == -1) {
-                k--;
-            }
-            if(e[v.top().first][v.top().second].num == -1) {
-                k_com--;
-            }
+            if(e[index_i][index_j].num == -1) k--;
+            index_i = s.top().first;
+            index_j = s.top().second;
         }
     }
     
-    while(!v.empty()) {
-        out << "(" << v.top().first << "," << v.top().second << ")";
-        v.pop();
-        if(!v.empty()) {
+    while(!s.empty()) {
+        out << "(" << s.top().first << "," << s.top().second << ")";
+        s.pop();
+        if(!s.empty()) {
             out << "->";
         }
     }
@@ -218,24 +195,13 @@ void path(Edge *e[]){
             if(e[i][j].num == -1) {
                 arr_slide(e,i,j);
             }
-            cout << "cost : ";
-            for(int k=0;k<M+N;k++) {
-                cout << " " << e[i][j].cost[k];
-            }
-            cout << endl;
-            
-            cout << "case : ";
-            for(int k=0;k<M+N;k++) {
-                cout << " " << e[i][j].num_case[k];
-            }
-            cout << endl;
-            cout << endl;
         }
     }
     for(int i=0;i<M+N;i++) {
         if(e[M-1][N-1].num_case[i] != 0) {
             out << "k:" << i << " count:" << e[M-1][N-1].num_case[i] << " cost:" << e[M-1][N-1].cost[i] << endl;
-            out_stack(e,i);
+            output_answer(e,i);
+            
         }
     }
 }
@@ -258,9 +224,12 @@ int main(void) {
                 e[i][j].num = 0;
                 e[i][j].cost = new int[M+N];
                 e[i][j].num_case = new int[M+N];
+                e[i][j].output_path = new pair<int,int>[M+N];
+                
                 for(int k=0;k<M+N;k++) {
                     e[i][j].cost[k] = 0;
                     e[i][j].num_case[k] = 0;
+                    e[i][j].output_path[k] = make_pair(-1, -1);
                 }
             }
         }
