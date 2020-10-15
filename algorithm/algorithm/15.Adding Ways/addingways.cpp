450205a4180437414ea377f59545d1eb8230e5ba
hogbal <hogbal31@gmail.com>
2020-10-15 23:58:43 +0900

    addingways

--- algorithm/algorithm/15.Adding Ways/addingways.cpp
+++ algorithm/algorithm/15.Adding Ways/addingways.cpp
@@ -5,40 +5,31 @@ using namespace std;
 ifstream inp("addingways.inp");
 ofstream out("addingways.out");
 
-int case_addingways;
-int *arr_case;
 int n,k;
 
-void addingways(int index){
-    if(index <= k-1) {
-        case_addingways++;
-        int give = 0;
-        int take = 1;
-        
-        while(1){
-            
-        }
-        
-        int sum = 0;
-        index++;
-        
-        for(int i=k-1;i>=0;i--) {
-            if(i == 0) {
-                arr_case[i] = n - sum;
-            }
-            else if(i <= index) {
-                arr_case[i] = 2;
+void addingways(){
+    int arr[301][301] = { 0 };
+    arr[1][1] = 1;
+    arr[2][1] = 1;
+    arr[2][2] = 1;
+    for(int i=3;i<=n;i++) {
+        for(int j=1;j<=i;j++) {
+            if(j==1 || i==j) {
+                arr[i][j] = 1;
             }
             else {
-                arr_case[i] = 1;
+                int sum = 0;
+                for(int k_temp=1;k_temp<=j;k_temp++) {
+                    if(k_temp > (i-j)) {
+                        break;
+                    }
+                    sum = ((arr[i-j][k_temp]%1000000007)+(sum%1000000007))%1000000007;
+                }
+                arr[i][j] = sum%1000000007;
             }
-            sum += arr_case[i];
-        }
-        
-        if(arr_case[0] >= arr_case[1]) {
-            addingways(index);
         }
     }
+    out << arr[n][k] << endl;
 }
 
 int main(void) {
@@ -47,30 +38,8 @@ int main(void) {
         if(n == 0 && k == 0) {
             break;
         }
-        else if(n < k) {
-            out << "0" << endl;
-        }
-        else if(k == 1) {
-            out << "1" << endl;
-        }
         else {
-            case_addingways = 0;
-            arr_case = new int[k];
-            
-            for(int i=k-1;i>=0;i--) {
-                if(i == 0) {
-                    arr_case[i] = n - (k-1);
-                }
-                else {
-                    arr_case[i] = 1;
-                }
-            }
-            
-            addingways(1);
-            
-            out << case_addingways << endl;
-            
-            delete[] arr_case;
+            addingways();
         }
     }
 }
